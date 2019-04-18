18f51b7a63668be1d294d891bcf700e2b4784538
AbdallahDrwesh <abdullah.drwesh98@eng-st.cu.edu.eg>
2019-04-18 20:24:25 +0200

    Edited

--- ByC++/3.BST-Set-Map/countDNA.cpp
+++ ByC++/3.BST-Set-Map/countDNA.cpp
@@ -9,16 +9,14 @@ int main(int argc, char **argv)
     {
         std::string dna = getFileFirstLine(argv[1]);
 
-        // Replace the `std::map` with your own dictionary that maps chars to integers.
         map::Map<char, int> dnaCounter;
-
+        
         for ( char &c : dna )
         {
-            // Use `dnaCounter.value( c )++` instead
+            
             dnaCounter.value(c)++;
         }
 
-        // Replace the for with `map::printAll( dnaCounter );`
         dnaCounter.printAll();
 
     }
--- ByC++/3.BST-Set-Map/count_words.cpp
+++ ByC++/3.BST-Set-Map/count_words.cpp
@@ -9,17 +9,14 @@ int main( int argc, char **argv )
     if( argc == 2 )
     {
         std::vector< std::string > words = getFileWords( argv[1] );
-
-        // Replace the `std::map` with your own dictionary that maps words to integers.
+        
         map::Map< std::string, int > wordMap;
 
         for( std::string &word : words )
         {
-            // Use `workMap.value( word )++` instead
             wordMap.value(word)++;
         }
 
-        // Replace the for with `map::printAll( wordMap );`
         wordMap.printAll();
         
     }
